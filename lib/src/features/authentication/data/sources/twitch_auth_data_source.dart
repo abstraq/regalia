051a79e0e9536091f3b4f591ee2c2bd5ea879998
abstraq <abstraq@outlook.com>
2022-05-12 19:50:39 -0400

    implemented secure storage data source

---
+++ lib/src/features/authentication/data/sources/twitch_auth_data_source.dart
@@ -0,0 +1,56 @@
+import "dart:developer" as developer;
+
+import "package:dio/dio.dart";
+
+import "../../domain/credentials.dart";
+import "../transfer/twitch_validation_response.dart";
+
+/// Data source that provides access to the Twitch ID API.
+///
+/// This data source is used to validate and revoke credentials.
+class TwitchAuthDataSource {
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: "https://id.twitch.tv/oauth2",
+      connectTimeout: 5000,
+      receiveTimeout: 3000,
+    ),
+  );
+
+  /// Checks if the token in the given [credentials] is valid.
+  ///
+  /// [credentials] are the [Credentials] to be validated.
+  ///
+  /// Throws a [DioError] if an error occurred while validating the credentials.
+  ///
+  /// Returns a [TwitchValidationResponse] if the credentials are valid.
+  Future<TwitchValidationResponse> validate(final Credentials credentials) async {
+    final response = await _dio.get(
+      "/validate",
+      options: Options(headers: {"Authorization": "OAuth ${credentials.token}"}),
+    );
+    final transferObject = TwitchValidationResponse.fromJson(response.data);
+    developer.log(
+      name: "TwitchAuthDataSource",
+      "Validated token in credentials for user ${credentials.userId}.",
+    );
+    return transferObject;
+  }
+
+  /// Revokes the token in the given [credentials].
+  ///
+  /// [credentials] are the [Credentials] to be revoked.
+  ///
+  /// Throws a [DioError] if an error occurred while revoking the credentials.
+  Future<void> revoke(final Credentials credentials) async {
+    await _dio.post(
+      "/revoke",
+      data: {"token": credentials.token, "client_id": credentials.clientId},
+      options: Options(contentType: "application/x-www-form-urlencoded"),
+    );
+    developer.log(
+      name: "TwitchAuthDataSource",
+      "Revoked token in credentials for user ${credentials.userId}.",
+    );
+  }
+}
