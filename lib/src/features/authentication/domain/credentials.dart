4a2ecffac3f024b42a1a6b4fb7b77984434e6015
abstraq <abstraq@outlook.com>
2022-05-12 19:51:51 -0400

    document credentials

--- lib/src/features/authentication/domain/credentials.dart
+++ lib/src/features/authentication/domain/credentials.dart
@@ -3,6 +3,10 @@ import "package:freezed_annotation/freezed_annotation.dart";
 part "credentials.freezed.dart";
 part "credentials.g.dart";
 
+/// Data class containing the credentials for a Twitch user.
+///
+/// This class contains the information obtained when validating the
+/// access token.
 @freezed
 class Credentials with _$Credentials {
   const factory Credentials({
