ea4bebf0b2548e88d748ad86c9c3e32b5250febd
abstraq <abstraq@outlook.com>
2022-05-30 14:45:40 -0400

    fix: lock the orientation in portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:regalia/routing/router.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const ProviderScope(child: RegaliaApp()));
 }
 
