a4fd6b4fe9d09f4ec82aa753be53f4d73521bc4b
abstraq <abstraq@outlook.com>
2022-06-21 23:50:05 -0400

    refactor(theme): changed colors

--- lib/core/presentation/default_theme.dart
+++ lib/core/presentation/default_theme.dart
@@ -4,33 +4,82 @@ import "package:regalia/core/presentation/shadows.dart";
 class DefaultTheme {
   const DefaultTheme._();
 
+  static const String fontFamily = "Lato";
+  static const Color primaryColor = Color(0xFF18643E);
+  static const Color foregroundColor = Color(0xFFF4F4F4);
+  static const Color backgroundColor = Color(0xFF212121);
+
   static const TextTheme textTheme = TextTheme(
-    headlineLarge: TextStyle(fontFamily: "Lato", fontSize: 48, fontWeight: FontWeight.w600),
-    headlineMedium: TextStyle(fontFamily: "Lato", fontSize: 34, fontWeight: FontWeight.w600),
-    headlineSmall: TextStyle(fontFamily: "Lato", fontSize: 20, fontWeight: FontWeight.w600),
-    titleLarge: TextStyle(fontFamily: "Lato", fontSize: 34, fontWeight: FontWeight.w500),
-    titleMedium: TextStyle(fontFamily: "Lato", fontSize: 24, fontWeight: FontWeight.w500),
-    titleSmall: TextStyle(fontFamily: "Lato", fontSize: 16, fontWeight: FontWeight.w500),
-    bodyLarge: TextStyle(fontFamily: "Lato", fontSize: 18, fontWeight: FontWeight.w400),
-    bodyMedium: TextStyle(fontFamily: "Lato", fontSize: 16, fontWeight: FontWeight.w400),
+    headlineLarge: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 48,
+      fontWeight: FontWeight.w600,
+    ),
+    headlineMedium: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 34,
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+    ),
+    titleLarge: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 34,
+      fontWeight: FontWeight.w500,
+    ),
+    titleMedium: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 24,
+      fontWeight: FontWeight.w500,
+    ),
+    titleSmall: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+    ),
+    bodyLarge: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+    ),
+    bodyMedium: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+    ),
     bodySmall: TextStyle(
-      fontFamily: "Lato",
+      fontFamily: fontFamily,
       fontSize: 14,
       fontWeight: FontWeight.w400,
       shadows: [AppShadows.darkElevation01],
     ),
-    labelLarge: TextStyle(fontFamily: "Lato", fontSize: 12, fontWeight: FontWeight.w500),
-    labelMedium: TextStyle(fontFamily: "Lato", fontSize: 11, fontWeight: FontWeight.w400),
-    labelSmall: TextStyle(fontFamily: "Lato", fontSize: 10, fontWeight: FontWeight.w400),
+    labelLarge: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+    ),
+    labelMedium: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+    ),
+    labelSmall: TextStyle(
+      fontFamily: fontFamily,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+    ),
   );
 
   static const ColorScheme darkColorScheme = ColorScheme.dark(
-    primary: Color.fromARGB(255, 218, 0, 55),
-    onPrimary: Color.fromARGB(255, 237, 237, 237),
-    background: Color.fromARGB(255, 50, 50, 50),
-    onBackground: Color.fromARGB(255, 237, 237, 237),
-    surface: Color.fromARGB(255, 40, 40, 40),
-    onSurface: Color.fromARGB(255, 237, 237, 237),
+    primary: primaryColor,
+    onPrimary: foregroundColor,
+    background: backgroundColor,
+    onBackground: foregroundColor,
+    surface: backgroundColor,
+    onSurface: foregroundColor,
   );
 
   static ThemeData darkTheme = ThemeData.from(colorScheme: darkColorScheme, textTheme: textTheme, useMaterial3: true);
